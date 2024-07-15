683bc789676234566a83374f7f1e678ee2746f76
VenkatachalamG02 <venkatachalamg02@gmail.com>
2024-07-15 10:19:48 +0530

    Added the program to find smallest character greater than the target

---
+++ Binary Search/src/ceilCharacter.java
@@ -0,0 +1,40 @@
+public class ceilCharacter {
+    public static void main(String[] args) {
+        char[] arr = {'c','e','f','g'};
+        char target = 'd';
+        char ans = orderAgnosticBinary(arr, target);
+        System.out.println(ans);
+    }
+
+    private static char orderAgnosticBinary(char[] arr, char target) {
+        int start = 0, end = arr.length - 1;
+
+        while (start <= end){
+            int mid = start + (end - start) / 2;
+
+            if (target == arr[mid]){
+                return arr[mid + 1];
+            }
+
+//            if(arr[start] < arr[end]){
+            if (target < arr[mid]){
+                end = mid - 1;
+            }
+            else if (target > arr[mid]){
+                start = mid + 1;
+            }
+//            }
+
+//            else {
+//                if (target > arr[mid]){
+//                    end = mid - 1;
+//                }
+//                else if (target < arr[mid]){
+//                    start = mid + 1;
+//                }
+//            }
+        }
+//        System.out.println(start);
+        return arr[start];
+    }
+}
